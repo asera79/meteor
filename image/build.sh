7e33d10230b4f0b02c93d9cfbaf157ea7cae4097
Sergey Antonov <sergey.antonov@bilot.fi>
2018-06-26 00:12:42 +0300

    MongoDB added

---
+++ image/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t asera79/meteor .
--- run.sh
+++ run.sh
@@ -16,6 +16,15 @@ if [ ! -d "$BIND_DIR" ] ; then
     echo "create project in new bind dir"
     docker run --rm -v "$BIND_DIR:/opt/app/$1" -w "/opt/app" asera79/meteor create $1
 fi
+echo "start mongodb"
+if [ ! "$(docker ps -q -f name=mongodb)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=mongodb)" ]; then
+        # cleanup
+        docker rm mongodb
+    fi
+    # run mongo container
+    docker run -d -v "mongodb:/data/db" -p 27017:27017 --name mongodb -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
+fi
 echo "start meteor"
 if [ ! "$(docker ps -q -f name=$1)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=$1)" ]; then
@@ -23,7 +32,7 @@ if [ ! "$(docker ps -q -f name=$1)" ]; then
         docker rm $1
     fi
     # run your container
-    docker run -d -v "$BIND_DIR:/opt/app/$1" -p 3000:3000 --name $1 -w "/opt/app/$1" asera79/meteor
+    docker run -d -v "$BIND_DIR:/opt/app/$1" -p 3000:3000 --name $1 -w "/opt/app/$1"  --link mongodb:mongodb asera79/meteor
 fi
 echo "docker exec -it $1 /bin/bash"
 
